74d45e230bc4cf0710f868af8aac3403daa02e75
vincent25sa <167078768+vincent25sa@users.noreply.github.com>
2026-05-22 01:55:56 +0200

    Fixed Render deployment port

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use("/api/admin", adminRoutes);
 app.use("/search", searchRoutes);
 app.use("/api/users", userRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
