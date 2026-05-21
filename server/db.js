c1b287e16375ab1a9370bc0ace6b6ef15928ea28
vincent25sa <167078768+vincent25sa@users.noreply.github.com>
2026-05-22 01:28:39 +0200

    Configured deployment environment

--- server/db.js
+++ server/db.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "vincent",
-  password: "system",
-  port: 3307
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 let resolveDbReady;
